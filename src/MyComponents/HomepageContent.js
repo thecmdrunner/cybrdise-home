d1cb3680978ce1eae867cce04e1b578d89b218f6
Pranav Kulkarni <gamerhat@pop-os.localdomain>
2021-07-08 17:45:05 +0530

    Removed unnecessary imports

--- src/MyComponents/HomepageContent.js
+++ src/MyComponents/HomepageContent.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 // import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+// import CardMedia from '@material-ui/core/CardMedia';
 // import Typography from '@material-ui/core/Typography';
 
 import { makeStyles } from '@material-ui/core/styles';
